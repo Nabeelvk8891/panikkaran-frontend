b1a369e1456afb4b69c81af18b8044c858a21a09
Nabeel vk <nabeelvk2006@gmail.com>
2026-01-29 17:13:34 +0530

    socket io fixed frontend

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_BACKEND_URL, {
-  transports: ["websocket", "polling"],
   withCredentials: true,
-  autoConnect: false,
 });
+
